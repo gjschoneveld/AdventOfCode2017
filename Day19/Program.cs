24fc212c5d56e9d72af30bc19ebb2dbe1e9a2428
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2017-12-21 18:19:42 +0100

    Day 19

---
+++ Day19/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19
+{
+    class Program
+    {
+        enum Direction
+        {
+            Down,
+            Right,
+            Up,
+            Left
+        }
+
+        class Location
+        {
+            public int x;
+            public int y;
+        }
+
+        static char GetItem(List<List<char>> diagram, int x, int y)
+        {
+            if (y < 0 || y >= diagram.Count)
+            {
+                return ' ';
+            }
+
+            if (x < 0 || x >= diagram[y].Count)
+            {
+                return ' ';
+            }
+
+            return diagram[y][x];
+        }
+
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var diagram = input.Select(line => line.ToList()).ToList();
+
+            var letters = new List<char>();
+
+            int y = 0;
+            int x = diagram[y].IndexOf('|');
+            var direction = Direction.Down;
+
+            int length = 0;
+            var current = GetItem(diagram, x, y);
+
+            while (current != ' ')
+            {
+                length++;
+
+                if (char.IsLetter(current))
+                {
+                    letters.Add(current);
+                }
+                else if (current == '+')
+                {
+                    if (direction == Direction.Down || direction == Direction.Up)
+                    {
+                        if (GetItem(diagram, x - 1, y) != ' ')
+                        {
+                            direction = Direction.Left;
+                        }
+                        else
+                        {
+                            direction = Direction.Right;
+                        }
+                    }
+                    else
+                    {
+                        if (GetItem(diagram, x , y - 1) != ' ')
+                        {
+                            direction = Direction.Up;
+                        }
+                        else
+                        {
+                            direction = Direction.Down;
+                        }
+                    }
+                }
+
+                switch (direction)
+                {
+                    case Direction.Down:
+                        y++;
+                        break;
+                    case Direction.Right:
+                        x++;
+                        break;
+                    case Direction.Up:
+                        y--;
+                        break;
+                    case Direction.Left:
+                        x--;
+                        break;
+                }
+
+                current = GetItem(diagram, x, y);
+            }
+
+            var answer1 = new String(letters.ToArray());
+            Console.WriteLine($"Answer 1: {answer1}");
+
+            var answer2 = length;
+            Console.WriteLine($"Answer 2: {answer2}");
+
+            Console.ReadKey();
+        }
+    }
+}
